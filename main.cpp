63be866d5ecd77dd78ccf1f9da222db994c845f3
mbisan <martin@bikandi.com>
2023-01-22 13:10:52 +0100

    version 0.7 secs

--- main.cpp
+++ main.cpp
@@ -9,6 +9,38 @@ using namespace std;
 typedef Eigen::MatrixXi Graph;
 typedef Eigen::VectorXi Perm;
 
+struct ls_output {
+    Perm perm;
+    int cost;
+};
+
+Perm loadPerm(string filename){
+    ifstream file;
+    file.open(filename);
+
+    string line;
+    if (file.is_open()){
+        getline(file, line);
+    } else {
+        file.close();
+        return Perm(0);
+    }
+
+    size_t len = stoi(line);
+
+    cout << "Loading Partition of size " << len << endl;
+
+    Perm perm = Perm::Constant(len, 0);
+
+    getline(file, line);
+
+    for (int i=0; i<len; i++) {
+        perm(i) = stoi(line.substr(i, 1));
+    }
+    file.close();
+    return perm;
+}
+
 Graph loadGraph(string filename){
     ifstream file;
     file.open(filename);
@@ -21,8 +53,6 @@ Graph loadGraph(string filename){
         return Graph(0, 0);
     }
 
-    cout << "Reading file -> ";
-
     string delimiter = " ";
 
     size_t token = stoi(line.substr(0, line.find(delimiter)));
@@ -30,11 +60,11 @@ Graph loadGraph(string filename){
 
     Graph graph = Graph::Constant(token, token, 0);
 
-    cout << "Graph with " << token << " vertices and ";
+    cout << "Loading Graph with " << token << " vertices and ";
 
     token = stoi(line.substr(0, line.find(delimiter)));
 
-    cout << token << " edges." << endl;
+    cout << token << " edges" << endl;
 
     size_t counter = 0;
     while(getline(file, line)){
@@ -56,6 +86,22 @@ Graph loadGraph(string filename){
     return graph;
 }
 
+int saveResults(ls_output res, string filename) {
+    ofstream pfile;
+    pfile.open(filename + ".p");
+
+    if (pfile.is_open()){
+        cout << "Saving resulting partition to " + filename + ".p" << endl;
+        int nvert = res.perm.size();
+        pfile << nvert << endl;
+        for (int i=0; i<nvert; i++){
+            pfile << res.perm(i);
+        }
+    }
+    pfile.close();
+    return 0;
+}
+
 int cost(Graph graph, Perm perm){
     int res = 0;
 
@@ -68,11 +114,6 @@ int cost(Graph graph, Perm perm){
     }
     return res;
 }
-
-struct ls_output {
-    Perm perm;
-    int cost;
-};
 
 auto localSearch(Graph graph, Perm perm, int n) {
     int cost_p = cost(graph, perm);
@@ -129,22 +170,41 @@ auto localSearch(Graph graph, Perm perm, int n) {
     return ls_output{best_p, best_c};
 }
 
-int main(){
-    int n = 1000;
-    Graph test = (Eigen::ArrayXXd::Random(n, n) > 0).cast<int>();
-    Perm testp = (Eigen::ArrayXd::Random(test.rows()) > 0).cast<int>();
+int main(int argc, char * argv[]){
+    if (argc % 2 == 0 || argc == 1) {
+        cout << "usage: (exec) -g GRAPH_FILE -p PARTITION_FILE -n ITERATIONS -o OUTPUT_FILE" << endl;
+        return 0;
+    }
 
-    // Graph test = loadGraph("test.txt");
-    // Perm testp(6);
-    // testp << 1, 1, 1, 0, 0, 0;
+    Graph test = (Eigen::ArrayXXd::Random(10, 10) > 0).cast<int>();
+    Perm testp;
+    int n = 1;
+    string output = "test";
 
-    cout << cost(test, testp) << endl;
+    for (int i=1; i<argc; i=i+2){
+        if(strcmp(argv[i], "-g")==0){
+            test = loadGraph(argv[i+1]);
+        } else if (strcmp(argv[i], "-p")==0){
+            testp = loadPerm(argv[i+1]);
+        } else if (strcmp(argv[i], "-n")==0){
+            n = stoi(argv[i+1]);
+        } else if (strcmp(argv[i], "-o")==0){
+            output = argv[i+1];
+        }
+    }
+
+    if (test.rows() != testp.size()) {
+        testp = (Eigen::ArrayXd::Random(test.rows()) > 0).cast<int>();
+    }
+
+    cout << " >> Start Local Search <<" << endl;
 
     chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    auto res = localSearch(test, testp, 1);
+    auto res = localSearch(test, testp, n);
     chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-    // cout << res.perm.transpose() << "\n" << res.cost << endl;
+    cout << "Finished local search in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " miliseconds" << endl;
 
-    cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << endl;
+    saveResults(res, output);
+    return 0;
 }
